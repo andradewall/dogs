c839cd469dba5f5d915a773a4a1114d742c82c59
wallace <wallace@pop-os.localdomain>
2022-04-09 21:56:21 -0300

    wip: login page

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -5,20 +5,23 @@ import LoginSignup from './LoginSignup'
 import LoginLostPassword from './LoginLostPassword'
 import LoginResetPassword from './LoginResetPassword'
 import { UserContext } from '../../UserContext'
+import styles from '../../css/Login.module.css'
 
 const Login = () => {
   const { isLogged } = useContext(UserContext)
 
   if (isLogged === true) return <Navigate to='/account' />
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<LoginForm />} />
-        <Route path='signup' element={<LoginSignup />} />
-        <Route path='lost-password' element={<LoginLostPassword />} />
-        <Route path='reset-password' element={<LoginResetPassword />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path='/' element={<LoginForm />} />
+          <Route path='signup' element={<LoginSignup />} />
+          <Route path='lost-password' element={<LoginLostPassword />} />
+          <Route path='reset-password' element={<LoginResetPassword />} />
+        </Routes>
+      </div>
+    </section>
   )
 }
 
--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -5,6 +5,9 @@ import Button from './../forms/Button'
 import useForm from '../../hooks/useForm'
 import { TOKEN_POST, USER_GET } from '../../api'
 import { UserContext } from '../../UserContext'
+import Error from '../../helpers/Error'
+import styles from '../../css/LoginForm.module.css'
+import stylesButton from '../../css/Button.module.css'
 
 const LoginForm = () => {
   // const [username, setUsername] = useState('')
@@ -23,9 +26,9 @@ const LoginForm = () => {
   }
 
   return (
-    <section>
-      <h1>Log in</h1>
-      <form action='' onSubmit={handleSubmit}>
+    <section className='animeLeft'>
+      <h1 className='title'>Log in</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input type='text' name='username' label='User' {...username} />
         <Input type='password' name='password' label='Password' {...password} />
         {loading ? (
@@ -33,9 +36,18 @@ const LoginForm = () => {
         ) : (
           <Button>Log in</Button>
         )}
-        {error && <p>{error}</p>}
+        <Error error={error} />
+        <Link className={styles.lost} to='/login/lost-password'>
+          Lost your password?
+        </Link>
       </form>
-      <Link to='/login/signup'>Sign up</Link>
+      <div className={styles.signup}>
+        <h2 className={styles.subtitle}>Sign up</h2>
+        <p>Create your account to show the world your pet!</p>
+        <Link className={stylesButton.button} to='/login/signup'>
+          Sign up
+        </Link>
+      </div>
     </section>
   )
 }
